ba30b9b409d905aea146339d701bafda3361963e
Jonathan H <jonathan.hoyos@utec.edu.pe>
2019-11-07 19:26:09 -0500

    modificaciones

---
+++ project/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "arbol.h"
+
+int main(){
+
+	Arbol arb;
+	string palabra;
+	cin>>palabra;
+	//arb.indermidiate_translate(palabra,english);
+	arb.translate(palabra,english,german);
+
+	return 0;
+}
